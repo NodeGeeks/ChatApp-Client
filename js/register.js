3d34f9e1263369308868abc3da8c3f263581b05a
aaronrussell <aaron@teknologenie.com>
2015-03-25 19:08:24 -0700

    changed to use remote server instead of local

--- js/register.js
+++ js/register.js
@@ -2,27 +2,35 @@ var registerForm = document.getElementById('registerForm');
 
 registerForm.addEventListener('submit', function(e){
     e.preventDefault();
+    var genderRadios = document.getElementsByName('gender'),
+        gender;
+
+    for(var i = 0; i < genderRadios.length; i++){
+        if(genderRadios[i].checked){
+            gender = genderRadios[i].value;
+        }
+    }
     var username = registerForm.username.value,
         firstName = registerForm.firstName.value,
         lastName = registerForm.lastName.value,
         email = registerForm.email.value,
-        //birthday = registerForm.birthday.value,
         password = registerForm.password.value;
 
-    register(firstName, lastName, username, email, password);
+    register(firstName, lastName, username, email, password, gender);
 });
 
-function register(id, firstName, lastName, username, email, password) {
+function register(firstName, lastName, username, email, password, gender) {
     var registerData = {
         firstName: firstName,
         lastName: lastName,
         username: username,
         email: email,
-        password: password
+        password: password,
+        gender: gender
     };
-
-    $.post( 'http://50.255.225.218/user/register', registerData).done(function( response ) {
-        var user = new User(data[0].id, data[0].firstName, data[0].lastName, data[0].username, data[0].email, true);
+    debugger
+    $.post( 'http://50.255.225.218/user/register', registerData).done(function( data ) {
+        var user = new User(data.id, data.firstName, data.lastName, data.username, data.email, true);
         localStorage.setItem('user', JSON.stringify(user));
         console.log(user);
 
